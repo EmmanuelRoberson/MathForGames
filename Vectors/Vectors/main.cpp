6517f0edf9b9e51f325f9761e170d9ebee51af21
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2018-12-10 10:50:35 -0600

    added a main, with test cases

---
+++ Vectors/Vectors/main.cpp
@@ -0,0 +1,15 @@
+#include "Vector2.h"
+#include "Vector3.h"
+
+int main()
+{
+	float five = 5.0f;
+	Vector2* test_vec2_1 = new Vector2(3.f, 3.f);
+	test_vec2_1*five;
+	test_vec2_1->GetX();
+	test_vec2_1->GetY();
+	
+	Vector2* test_vec2_2= new Vector2(10.f, 10.f);
+
+
+}
