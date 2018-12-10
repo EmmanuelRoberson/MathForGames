1d60a25c25b2a00eaf025226b790d30c7b0f562f
emmanuel.roberson3 <emmanuel.roberson3@gmail.com>
2018-12-10 15:23:07 -0600

    added test cases for the distance functions for vector2 and vector3

--- Vectors/Vectors/main.cpp
+++ Vectors/Vectors/main.cpp
@@ -4,14 +4,15 @@
 int main()
 {
 	float five = 5.0f;
+
 	Vector2 test_vec2_1 = Vector2(6, 9);
+	Vector2 test_vec2_2 = Vector2(1, 9);
 	test_vec2_1.GetX();
 	test_vec2_1.GetY();
 
-	test_vec2_1.Normalize();
-
 	Vector3 test_vec3_1 = Vector3(1.0, 1.0, 1.0);
-	test_vec3_1.Normalize();
+	Vector3 test_vec3_2 = Vector3(1.0, 1.0, 10);
 
-
+	float distance = test_vec2_1.Distance(test_vec2_2);
+	float distance2 = test_vec3_1.Distance(test_vec3_2);
 }
